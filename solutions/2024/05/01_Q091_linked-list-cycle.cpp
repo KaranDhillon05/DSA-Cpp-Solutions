9236da56b30c94ce3ea94577c7b4e090098c2b3c
Karan Dhillon <kd2803@srmist.edu.in>
2024-05-01 10:49:57 +0530

    solve: Q091 Linked List Cycle [Easy]

---
+++ solutions/2024/05/01_Q091_linked-list-cycle.cpp
@@ -0,0 +1,33 @@
+/*
+ * Day: 91 | Date: 2024-05-01
+ * Q091: Linked List Cycle
+ * Difficulty: Easy
+ * Pattern: Fast & Slow Pointers
+ * Section: 3.1
+ *
+ * Daily DSA practice — FAANG curated set (1 problem / day from 2024-02-01)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
