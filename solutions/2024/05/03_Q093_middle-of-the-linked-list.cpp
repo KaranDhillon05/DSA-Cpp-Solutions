e3a2d46771d990573c990591c9bc3c60cece3276
Karan Dhillon <kd2803@srmist.edu.in>
2024-05-03 20:10:41 +0530

    solve: Q093 Middle of the Linked List [Easy]

---
+++ solutions/2024/05/03_Q093_middle-of-the-linked-list.cpp
@@ -0,0 +1,32 @@
+/*
+ * Day: 93 | Date: 2024-05-03
+ * Q093: Middle of the Linked List
+ * Difficulty: Easy
+ * Pattern: Fast & Slow Pointers
+ * Section: 3.1
+ *
+ * Daily DSA practice — FAANG curated set (1 problem / day from 2024-02-01)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
