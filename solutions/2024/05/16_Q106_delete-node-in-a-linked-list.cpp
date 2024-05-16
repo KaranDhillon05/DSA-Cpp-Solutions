7c70b0cb728c00660fa592823893768180c576a5
Karan Dhillon <kd2803@srmist.edu.in>
2024-05-16 11:44:49 +0530

    solve: Q106 Delete Node in a Linked List [Medium]

---
+++ solutions/2024/05/16_Q106_delete-node-in-a-linked-list.cpp
@@ -0,0 +1,28 @@
+/*
+ * Day: 106 | Date: 2024-05-16
+ * Q106: Delete Node in a Linked List
+ * Difficulty: Medium
+ * Pattern: Linked List Manipulation
+ * Section: 3.2
+ *
+ * Daily DSA practice — FAANG curated set (1 problem / day from 2024-02-01)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
