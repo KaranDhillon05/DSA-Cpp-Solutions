34265c388a32c898b6f95bf8149f44c30191a43e
Karan Dhillon <kd2803@srmist.edu.in>
2024-05-17 16:46:36 +0530

    solve: Q107 Odd Even Linked List [Medium]

---
+++ solutions/2024/05/17_Q107_odd-even-linked-list.cpp
@@ -0,0 +1,36 @@
+/*
+ * Day: 107 | Date: 2024-05-17
+ * Q107: Odd Even Linked List
+ * Difficulty: Medium
+ * Pattern: Linked List Manipulation
+ * Section: 3.2
+ *
+ * Daily DSA practice — FAANG curated set (1 problem / day from 2024-02-01)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return nullptr;
+        ListNode *odd = head, *even = head->next, *evenHead = even;
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
