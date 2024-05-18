1ada8a13a78fa8d3ac85ac426a284cd07666029e
Karan Dhillon <kd2803@srmist.edu.in>
2024-05-18 15:16:19 +0530

    solve: Q108 Intersection of Two Linked Lists [Easy]

---
+++ solutions/2024/05/18_Q108_intersection-of-two-linked-lists.cpp
@@ -0,0 +1,32 @@
+/*
+ * Day: 108 | Date: 2024-05-18
+ * Q108: Intersection of Two Linked Lists
+ * Difficulty: Easy
+ * Pattern: Two Pointers
+ * Section: 3.2
+ *
+ * Daily DSA practice — FAANG curated set (1 problem / day from 2024-02-01)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a = headA, *b = headB;
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+        return a;
+    }
+};
