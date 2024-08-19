f57c61d5a53656c1e861e7149ac0b16ff92fef51
Karan Dhillon <kd2803@srmist.edu.in>
2024-08-19 10:17:27 +0530

    solve: Q201 Lowest Common Ancestor of a BST [Easy]

---
+++ solutions/2024/08/19_Q201_lowest-common-ancestor-of-a-bst.cpp
@@ -0,0 +1,33 @@
+/*
+ * Day: 201 | Date: 2024-08-19
+ * Q201: Lowest Common Ancestor of a BST
+ * Difficulty: Easy
+ * Pattern: BST
+ * Section: Trees
+ *
+ * Daily DSA practice — FAANG curated set (1 problem / day from 2024-02-01)
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *l, TreeNode *r) : val(x), left(l), right(r) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if (p->val < root->val && q->val < root->val) root = root->left;
+            else if (p->val > root->val && q->val > root->val) root = root->right;
+            else return root;
+        }
+        return nullptr;
+    }
+};
